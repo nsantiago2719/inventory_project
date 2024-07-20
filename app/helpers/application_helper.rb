941cd74b5ef16d3c91a8516b56ef29eda3c50fcb
norman santiago <nsantiago@ethernalabs.com>
2024-07-20 20:15:30 +0800

    update tailwind_button to accept optional classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def tailwind_button(name, path)
-    link_to name, path, class: "bg-blue-500 rounded no-underline text-blue-100 px-4 py-2"
+  def tailwind_button(name, path, additional_class="")
+    link_to name, path, class: "bg-blue-500 rounded no-underline text-blue-100 px-4 py-2 #{additional_class}"
   end
 
   def notif_color(flash_type)
