ce2942110dcc1f714fe3dc4d9a8f2f70e3d4d723
norman santiago <nsantiago@ethernalabs.com>
2024-07-20 19:28:30 +0800

    added different notif styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def tailwind_button(name, path)
     link_to name, path, class: "bg-blue-500 rounded no-underline text-blue-100 px-4 py-2"
   end
+
+  def notif_color(flash_type)
+    if flash_type == "notice"
+      ["bg-green-300", "text-green-950"]
+    else
+      ["bg-red-300", "text-red-950"]
+    end
+  end
 end
