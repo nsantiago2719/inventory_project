d09d49bb157981eeb74c7ae3c3870390f1d1b682
norman santiago <nsantiago@ethernalabs.com>
2024-07-23 18:47:49 +0800

    remove webauthn import

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import WebAuthnController from "stimulus-web-authn"
 
 const application = Application.start()
-
-application.register("web-authn", WebAuthnController)
 
 // Configure Stimulus development experience
 application.debug = false
