c4b876f0c955b6efaf5c691c1a1d670604bef5f9
norman santiago <nsantiago@ethernalabs.com>
2024-07-27 19:45:21 +0800

    update item show page for rendering description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def tailwind_button(name, path)
-    link_to name, path, class: "bg-blue-500 rounded no-underline text-white px-4 py-2 rounded-md font-semibold shadow-sm"
+    link_to name, path, class: "bg-blue-500 rounded no-underline text-white px-4 py-2 rounded-md font-semibold shadow-sm w-full block text-center"
   end
 
   def notif_color(flash_type)
